af4226d979ba414d9ee86090c4651e94c0f3a109
Veugelen Joeri <jv120501@stud.miniemeninstituut.be>
2019-01-17 11:36:22 +0100

    bg

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
 
 <div class="center">
 <!-- Het logo -->
-<div class="logo-container"><a href="https://mwnb.nl/"><img class='logo' src="Monumentenwacht_NB_icon_DEF_transparant.png" alt="Logo monumentenwacht"></a></div>
+<div class="logo-container"><a href="https://mwnb.nl/"><img class='logo' src="Images\Monumentenwacht_NB_icon_DEF_transparant.png" alt="Logo monumentenwacht"></a></div>
 
 <div class="time-container">
 <?php
