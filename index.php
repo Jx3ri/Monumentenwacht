596ce28e41c8807b1bbc1bc8866f0a6289f3b379
Mike Van Camp <mikevc@live.be>
2019-03-26 10:40:34 +0100

    .

--- index.php
+++ index.php
@@ -62,7 +62,7 @@ $hour = date('H', time());
     
 ?>
 
-</form method="POST" action="submit.php">
+<form id="comment-form" method="post" action="submit.php">
     <textarea name="comment" id="comment" placeholder="Laat nog een opmerking achter over uw monument.."></textarea>
     <div class="button-container">
         <a href="#" class="btn btn-5" onclick="document.getElementById('comment-form').submit()">Verzenden</a>
