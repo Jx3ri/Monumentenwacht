fcecec11a8b4713ef44d38113587ece7220517ba
Mike Van Camp <mikevc@live.be>
2019-03-26 09:40:07 +0100

    update

---
+++ confirmed.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Klaar</title>
+    <link rel="stylesheet" href="CSS/style.css">
+</head>
+<body>
+
+<div class="center-container">
+<div class="center cover">
+    <div class="logo-container"><a href="https://mwnb.nl/"><img class='logo' src="Images\Monumentenwacht_NB_logo_DEF-400x250.png" alt="Logo monumentenwacht"></a></div>
+    <div class="time-container">
+    <h1>Uw antwoord is verzonden, u kan deze browser tab sluiten.</h1>
+    </div>
+</div>
+</div>
+
+</body>
+</html>
