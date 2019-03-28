d22b7c1280650ae1d2f7a0ee09154eb0ee7156cf
Jx3ri <45363358+Jx3ri@users.noreply.github.com>
2019-03-28 12:05:49 +0100

    kaas

--- reminder.php
+++ reminder.php
@@ -1,3 +1,26 @@
 <?php
+include('db_connect.php');
+date_default_timezone_set('Europe/Amsterdam');
+$datum = date("Y/m/d");
+
+
+$datetime1 = new DateTime($datum);
+$datetime2 = new DateTime('2009-10-13');
+$interval = $datetime1->diff($datetime2);
+echo $interval->format('%a');
+
+$query=$conn->prepare('SELECT * FROM tblplanningantwoord');
+$query->execute();
+$result=$query->fetchAll(PDO::FETCH_ASSOC);
+foreach($result as &$data){
+    $TijdstipGecontacteerd = $data['fldTijdstipGecontacteerd'];
+    $ReminderTijdstip = date('Y-m-d', strtotime($TijdstipGecontacteerd. ' + 4 days'));
+    echo "<br>";
+    echo $TijdstipGecontacteerd;
+    echo "<br>";
+    echo $ReminderTijdstip;
+
+}
+
 
 ?>
